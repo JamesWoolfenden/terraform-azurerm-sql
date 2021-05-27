97defc90f692d0a3ca7fdd1e6303e9f2f23877ee
James Woolfenden <james.woolfenden@gmail.com>
2021-05-27 14:29:41 +0100

    refresh

--- example/examplea/azurerm_log_analytics_storage_insights.example.tf
+++ example/examplea/azurerm_log_analytics_storage_insights.example.tf
@@ -1,7 +1,7 @@
 resource "azurerm_log_analytics_storage_insights" "example" {
   name                = "example-storageinsightconfig"
-  resource_group_name = var.resource_group.name
-  workspace_id        = azurerm_log_analytics_workspace.example.id
+  resource_group_name = azurerm_resource_group.examplea.name
+  workspace_id        = azurerm_log_analytics_workspace.examplea.id
 
   storage_account_id   = azurerm_storage_account.logging.id
   storage_account_key  = azurerm_storage_account.logging.primary_access_key
